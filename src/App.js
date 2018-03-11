9ee452ae3c0440d05a375b2f8f177c398f09229c
ignacio bochatay <ignaciobochatay@gmail.com>
2018-03-11 17:29:27 -0300

    adding scroll

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import './App.css';
 
 class App extends Component {
@@ -34,7 +35,9 @@ class App extends Component {
         <div className="tc">
           <h1 className="f1">Monster List</h1>
           <SearchBox searchChange={this.onSearchChange}/>
-          <CardList monsters={filteredMonsters} />
+          <Scroll>
+            <CardList monsters={filteredMonsters} />
+          </Scroll>
         </div>
       );
     }
